22b630b40ed3698312acd163e52e3628853e56be
Shirisha <sirisha.pcsr@gmail.com>
2024-09-23 23:02:59 +0530

    added menu overlay trnasition

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,4 +1,6 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 import logo from "../../Assets/Images/Logo/logo.svg";
 import menu from "../../Assets/Images/Logo/menu.svg";
 import close from "../../Assets/Images/Logo/closeIcon.svg";
@@ -7,6 +9,7 @@ import styles from "./Header.module.scss";
 export default function Header(props) {
   const { scrollToSection, homeRef, aboutRef, skillsRef, workRef, contactRef } =
     props;
+  const overlayRef = useRef(null);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const handleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
@@ -15,6 +18,20 @@ export default function Header(props) {
     handleMenu();
     scrollToSection(ref);
   };
+
+  useGSAP(
+    () => {
+      if (isMenuOpen) {
+        gsap.to(overlayRef.current, {
+          top: "0%",
+        });
+      } else {
+        gsap.to(overlayRef.current, { top: "-100%" });
+      }
+    },
+    { dependencies: [isMenuOpen], scope: overlayRef }
+  );
+
   return (
     <>
       <div className={styles.headerMainClass}>
@@ -28,22 +45,20 @@ export default function Header(props) {
         </div>
       </div>
       <>
-        {isMenuOpen && (
-          <div className={styles.navbarContainer}>
-            <div onClick={handleMenu} className={styles.closeIcon}>
-              <img src={close} alt="close" />
-            </div>
-            <div className={styles.wrapper}>
-              <ul>
-                <li onClick={() => handleClick(homeRef)}>Home</li>
-                <li onClick={() => handleClick(aboutRef)}>About Me</li>
-                <li onClick={() => handleClick(skillsRef)}>Experience</li>
-                <li onClick={() => handleClick(workRef)}>Projects</li>
-                <li onClick={() => handleClick(contactRef)}>Contact</li>
-              </ul>
-            </div>
+        <div ref={overlayRef} className={styles.navbarContainer}>
+          <div onClick={handleMenu} className={styles.closeIcon}>
+            <img src={close} alt="close" />
           </div>
-        )}
+          <div className={styles.wrapper}>
+            <ul>
+              <li onClick={() => handleClick(homeRef)}>Home</li>
+              <li onClick={() => handleClick(aboutRef)}>About Me</li>
+              <li onClick={() => handleClick(skillsRef)}>Experience</li>
+              <li onClick={() => handleClick(workRef)}>Projects</li>
+              <li onClick={() => handleClick(contactRef)}>Contact</li>
+            </ul>
+          </div>
+        </div>
       </>
     </>
   );
