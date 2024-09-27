01205302760d9e7e31348636fc2a9f264b0bbd1a
Shirisha <sirisha.pcsr@gmail.com>
2024-09-27 21:21:30 +0530

    added responsive issue

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ function App() {
     () => {
       gsap.to(".loadingAnimationStyles", {
         delay: 1.5,
-        top: "-125%",
+        top: "-100%",
         duration: 2,
       });
     },
